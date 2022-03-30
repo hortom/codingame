8bee91729babe8e66e04eff5193cdb9895c9e273
Tamas Hortobagyi <hortobagyi.tom@gmail.com>
2022-03-30 18:45:05 +0100

    Update CodinGame.hx
    NodeJS, PHP, Python, Lua support

--- CodinGame.hx
+++ CodinGame.hx
@@ -1,8 +1,12 @@
 package;
-
 /**
- * ...
- * @author Tom Hortobágyi
+ * @author Tamas Hortobágyi
+ * 
+ * History:
+ *  v0.1: JavaScript support
+ *  v0.2: NodeJS, PHP, Python, Lua support
+ *
+ * note: Big thanks to https://github.com/0xA3 for adding the other platforms than JS.
  */
 /*
 @:native("")
@@ -15,9 +19,40 @@ extern class CodinGame
 //*/
 class CodinGame
 {
-	#if js
-	static public function print(output:Dynamic):Void { untyped __js__('print(output)'); }
-	static public function printErr(output:Dynamic):Void { untyped __js__('printErr(output)'); }
-	static public inline function readline():String { return untyped __js__('readline()'); }
+	#if nodejs
+	static public function print(output:Dynamic):Void { Sys.println(output); }
+	static public function printErr(output:Dynamic):Void { Sys.println(output); }
+	static public inline function readline():String { return Sys.stdin().readLine(); }
+	#elseif js
+	static public function print(output:Dynamic):Void { js.Syntax.code('print(output)'); }
+	static public function printErr(output:Dynamic):Void { js.Syntax.code('printErr(output)'); }
+	static public inline function readline():String { return js.Syntax.code('readline()'); }
+	#elseif php
+	static public function print(output:Dynamic):Void { untyped __php__('echo "$output\n"'); }
+	static public function printErr(output:Dynamic):Void { untyped __php__('error_log(var_export($output, true)'); }
+	static public inline function readline():String { return untyped __php__('fscanf(STDIN, "%d", $N);'); }
+	#elseif python
+	static public function print(output:Dynamic):Void { python.Syntax.code('print(output)'); }
+	static public function printErr(output:Dynamic) {
+            python.Syntax.code('print(output, file=python_lib_Sys.stderr)');
+            return python.lib.Sys.platform; // to force import of sys in python script
+        }
+	static public inline function readline():String { return python.Syntax.code('input()'); }
+	#elseif lua
+	static public function print(output:Dynamic):Void { untyped __lua__('print(output)'); }
+	static public function printErr(output:Dynamic):Void { untyped __lua__('io.stderr:write(output)'); }
+	static public inline function readline():String { return untyped __lua__('io.read()'); }
+	#elseif interp
+	static public function print(output:Dynamic):Void { Sys.println(output); }
+	static public function printErr(output:Dynamic):Void { Sys.println(output); }
+	static public inline function readline():String { return ""; }
+	#elseif sys
+	static public function print(output:Dynamic):Void { Sys.println(output); }
+	static public function printErr(output:Dynamic):Void { Sys.println(output); }
+	static public inline function readline():String { return Sys.stdin().readLine(); }
+	#else
+	static public function print(output:Dynamic):Void { trace(output); }
+	static public function printErr(output:Dynamic):Void { trace(output); }
+	static public inline function readline():String { return ""; }
 	#end
 }
